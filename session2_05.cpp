b7f2c52d601f05eca17f26177c6fe7365b56317d
tiendat <dat.70.k3@gmail.com>
2024-11-20 21:51:01 +0700

    bai 5

---
+++ session2_05.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main( ){
+	int Canh = 3, ChuVi, DienTich;
+	ChuVi = Canh * 4;
+	DienTich = Canh * Canh;
+	
+	printf("Chu vi hinh vuong la %d \n", ChuVi);
+	printf("Dien tich hinh vuong la %d \n", DienTich);
+	return 0;
+}
+	
+
